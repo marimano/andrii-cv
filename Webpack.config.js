5bc9901ceea81670284cf4a31b5e77ad86506f3c
Maria <marfayaga@gmail.com>
2023-05-01 21:04:29 +0300

    update entry to js

--- Webpack.config.js
+++ Webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.jsx'),
+    entry: path.resolve(__dirname, './src/index.js'),
     mode: 'development',
     output: {
         path: path.resolve(__dirname,'dist'),
