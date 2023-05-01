86a9b1f8ac84262c767cdd44457a7c84d75a17b7
Maria <marfayaga@gmail.com>
2023-05-01 21:15:37 +0300

    Revert "fdssdfsdf"
    This reverts commit f6548b23053b8198e049d3bfff5df235c7aa7293.

--- Webpack.config.js
+++ Webpack.config.js
@@ -4,16 +4,13 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.jsx'),
+    entry: path.resolve(__dirname, './src/index.js'),
     mode: 'development',
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: '[name][fullhash].js',
         clean: true,
         publicPath: '/',
-    },
-    resolve: {
-      extensions: ['*', '.js', '.jsx'],
     },
     plugins: [
         new HtmlWebpackPlugin({
