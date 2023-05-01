3d01732b35dac36418d5a366424159f8cc5aa4bb
Maria <marfayaga@gmail.com>
2023-05-01 22:55:47 +0300

    костиль

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-import './index.scss'
-import React from 'react'
-import ReactDom from 'react-dom/client'
-import App from './App.jsx'
-import ErrorBoundary from "./components/ErrorBoundary.jsx";
-import {BrowserRouter} from "react-router-dom";
-
-const rootEl = document.getElementById('root')
-const root = ReactDom.createRoot(rootEl)
-
-root.render(<BrowserRouter>
-    <ErrorBoundary>
-    <App/>
-    </ErrorBoundary>
-    </BrowserRouter>)
+import './index.jsx'
